25dd29a151761f5ed9a3131d6b355897d5c541fa
Constança Cunha <79765263+constancadcunha@users.noreply.github.com>
2025-04-27 19:04:27 +0100

    Update FavoriteSongListApp.swift

--- CottageTunes/FavoriteSongListApp.swift
+++ CottageTunes/FavoriteSongListApp.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 @main
 struct FavoriteSongListApp: App {
-    // Remove the PersistenceController reference
+    
     @StateObject private var viewModel = MusicViewModel()
     
     var body: some Scene {
